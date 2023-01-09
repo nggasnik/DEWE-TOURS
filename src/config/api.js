6f90b89b8e274d2de8405914bc0f1d30c234442f
Erlangga <110957992+nggasnik@users.noreply.github.com>
2023-01-09 11:11:19 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://be-production-8969.up.railway.app//api/v1/",
+  baseURL: "https://be-production-ffbd.up.railway.app/api/v1/",
 });
 
 export const setAuthToken = (token) => {
